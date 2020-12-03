5befebe5aa90a33e28f12b56b9512154ad1011d2
Valerie Montoya <valeriemontoya90@gmail.com>
2020-12-04 00:03:19 +0100

    v0: reset css and styles shared

--- ui/src/index.js
+++ ui/src/index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
 import App from "./App";
+import { GlobalStyle } from "./utils/globalStyle";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>,
   document.getElementById("root")
