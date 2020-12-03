70a689ad0f7650ed71c2f85b1cc573355b856b7e
Valerie Montoya <valeriemontoya90@gmail.com>
2020-12-04 00:05:28 +0100

    v0: add .stylelintrc.js

---
+++ ui/.stylelintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  processors: ['stylelint-processor-styled-components'],
+  extends: [
+    'stylelint-config-recommended',
+    'stylelint-config-styled-components'
+  ],
+  rules: {
+    'color-no-invalid-hex': true,
+    'value-list-comma-space-after': 'always',
+    'length-zero-no-unit': true
+  }
+};
