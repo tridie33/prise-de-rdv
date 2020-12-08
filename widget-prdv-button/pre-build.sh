c2ff7c2eb4c4c492c0e6189a6eda1a6e9b9a6237
Valerie Montoya <valeriemontoya90@gmail.com>
2020-12-09 00:38:53 +0100

    v0: fix post-merge hook script for linux

--- widget-prdv-button/pre-build.sh
+++ widget-prdv-button/pre-build.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
+echo "Current Working Directory: $PWD"
+echo "GIT_DIR=$GIT_DIR"
+# Needed to avoid "not a git repositoty" error
+unset GIT_DIR
 
-if yarn isBranch master; then 
+if yarn isBranch master; then
   yarn build:production
   git add ../ui/public/assets/bundle.js
 fi
-if yarn isBranch recette; then 
+if yarn isBranch recette; then
   yarn build:recette
   git add ../ui/public/assets/bundle.js
 fi
