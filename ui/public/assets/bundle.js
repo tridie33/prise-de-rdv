bd8ff9a52bdfcc689c2db486e9f28736c5d8c4af
Valerie Montoya <valeriemontoya90@gmail.com>
2020-12-14 10:31:32 +0100

    v1: get new impl widget prdv

--- ui/public/assets/bundle.js
+++ ui/public/assets/bundle.js
@@ -12,13 +12,87 @@
     globalThis.process = { env:env };
 })();
 
+function _unsupportedIterableToArray(o, minLen) {
+  if (!o) return;
+  if (typeof o === "string") return _arrayLikeToArray(o, minLen);
+  var n = Object.prototype.toString.call(o).slice(8, -1);
+  if (n === "Object" && o.constructor) n = o.constructor.name;
+  if (n === "Map" || n === "Set") return Array.from(o);
+  if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen);
+}
+
+function _arrayLikeToArray(arr, len) {
+  if (len == null || len > arr.length) len = arr.length;
+
+  for (var i = 0, arr2 = new Array(len); i < len; i++) arr2[i] = arr[i];
+
+  return arr2;
+}
+
+function _createForOfIteratorHelper(o, allowArrayLike) {
+  var it;
+
+  if (typeof Symbol === "undefined" || o[Symbol.iterator] == null) {
+    if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") {
+      if (it) o = it;
+      var i = 0;
+
+      var F = function () {};
+
+      return {
+        s: F,
+        n: function () {
+          if (i >= o.length) return {
+            done: true
+          };
+          return {
+            done: false,
+            value: o[i++]
+          };
+        },
+        e: function (e) {
+          throw e;
+        },
+        f: F
+      };
+    }
+
+    throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.");
+  }
+
+  var normalCompletion = true,
+      didErr = false,
+      err;
+  return {
+    s: function () {
+      it = o[Symbol.iterator]();
+    },
+    n: function () {
+      var step = it.next();
+      normalCompletion = step.done;
+      return step;
+    },
+    e: function (e) {
+      didErr = true;
+      err = e;
+    },
+    f: function () {
+      try {
+        if (!normalCompletion && it.return != null) it.return();
+      } finally {
+        if (didErr) throw err;
+      }
+    }
+  };
+}
+
 if (document.readyState !== 'loading') {
   console.log('document is already ready, just execute code here');
-  myInitCode();
+  loaderWidgetPRDV();
 } else {
   document.addEventListener('DOMContentLoaded', function () {
     console.log('document was not ready, place code here !');
-    myInitCode();
+    loaderWidgetPRDV();
   });
 }
 
@@ -36,6 +110,7 @@ switch (prdv_mna_env) {
     break;
 
   case "local":
+    prdv_mna_hostname = "http://localhost";
     break;
 
   default:
@@ -43,52 +118,41 @@ switch (prdv_mna_env) {
     break;
 }
 
-function myInitCode() {
-  if (document.getElementById("prdv-button") !== null) {
-    var urlHost = window.location.href;
-    var valueCentreId = null;
-    var valueTrainingId = null;
-    var fromReferrer = null;
+function loaderWidgetPRDV() {
+  var elements = document.getElementsByClassName('widget-prdv');
 
-    if (urlHost === prdv_mna_hostname + '/fakeHost/ps') {
-      /*
-      var getElementByIdForPs = document.getElementById('domPSCentreId');
-      if (getElementByIdForPs !== null) {
-          valueCentreId = getElementByIdForPs.textContent;
-      }
-      var getElementByTrainingIdForPs = document.getElementById('domPSTrainingId');
-      if (getElementByTrainingIdForPs !== null) {
-          valueTrainingId = getElementByTrainingIdForPs.textContent;
-      }
-       */
-      valueCentreId = "0831760M";
-      valueTrainingId = "13531545";
-      fromReferrer = "Parcoursup";
+  var _iterator = _createForOfIteratorHelper(elements),
+      _step;
+
+  try {
+    for (_iterator.s(); !(_step = _iterator.n()).done;) {
+      var element = _step.value;
+      createWidgetPRDV(element);
     }
+  } catch (err) {
+    _iterator.e(err);
+  } finally {
+    _iterator.f();
+  }
+}
 
-    if (urlHost === prdv_mna_hostname + '/fakeHost/lba') {
-      /*
-      var getElementByIdForLBA = document.getElementById('domLBACentreId');
-      if (getElementByIdForLBA !== null) {
-          valueCentreId = getElementByIdForLBA.textContent;
-      }
-      var getElementByTrainingIdForLBA = document.getElementById('domLBATrainingId');
-      if (getElementByTrainingIdForLBA !== null) {
-          valueTrainingId = getElementByTrainingIdForLBA.textContent;
-      }
-       */
-      valueCentreId = "0831760M";
-      valueTrainingId = "13531545";
-      fromReferrer = "LBA";
-    }
-
+function createWidgetPRDV(element) {
+  if (element !== null) {
+    var fromReferrer = window.location.href;
+    var valueCentreId = element.dataset.prdvCentre !== undefined ? element.dataset.prdvCentre : null;
+    var valueTrainingId = element.dataset.prdvTraining !== undefined ? element.dataset.prdvTraining : null;
+    var valueSiteName = element.dataset.prdvSitename !== undefined ? element.dataset.prdvSitename : window.location.origin;
+    var valueCandidatFirstname = element.dataset.prdvCandidatFirstname !== undefined ? element.dataset.prdvCandidatFirstname : undefined;
+    var valueCandidatLastname = element.dataset.prdvCandidatLastname !== undefined ? element.dataset.prdvCandidatLastname : undefined;
     var a = document.createElement('a');
     var link = document.createTextNode("Prendre rendez-vous");
     a.appendChild(link);
     a.title = "Prendre rendez-vous";
-    a.href = "".concat(prdv_mna_hostname, "/form?fromReferrer=").concat(fromReferrer, "&centreId=").concat(valueCentreId, "&trainingId=").concat(valueTrainingId);
+    a.href = "".concat(prdv_mna_hostname, "/form?fromReferrer=").concat(fromReferrer, "&centreId=").concat(valueCentreId, "&trainingId=").concat(valueTrainingId, "&siteName=").concat(valueSiteName, "&candidatFirstname=").concat(valueCandidatFirstname, "&candidatLastname=").concat(valueCandidatLastname);
     var button = document.createElement('button');
     button.appendChild(a);
-    document.getElementById("prdv-button").appendChild(button);
+    element.appendChild(button);
+  } else {
+    console.log("error loading widget PRDV");
   }
 }
