91f6ebf666175e8e7d4b3792b6ea85b3d9ea908e
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2021-09-16 21:42:31 +0200

    feat(server/src/http/routes/admin/appointment.js): dont format sib datetime

--- server/src/http/routes/admin/appointment.js
+++ server/src/http/routes/admin/appointment.js
@@ -172,9 +172,9 @@ module.exports = ({ cache }) => {
           email_premiere_demande_candidat_statut: getEmailStatus(document?.email_premiere_demande_candidat_statut),
           email_premiere_demande_candidat_statut_date:
             formatDate(document.email_premiere_demande_candidat_statut_date) || "N/C",
-          email_premiere_demande_cfa_date: formatDate(document.email_premiere_demande_cfa_date) || "N/C",
+          email_premiere_demande_cfa_date: document.email_premiere_demande_cfa_date || "N/C",
           email_premiere_demande_cfa_statut: getEmailStatus(document?.email_premiere_demande_cfa_statut),
-          email_premiere_demande_cfa_statut_date: formatDate(document.email_premiere_demande_cfa_statut_date) || "N/C",
+          email_premiere_demande_cfa_statut_date: document.email_premiere_demande_cfa_statut_date || "N/C",
           cfa_pris_contact_candidat_date: formatDate(document.cfa_pris_contact_candidat_date) || "N/C",
           source: getReferrerById(document.referrer).full_name,
           motivation: document.motivations,
