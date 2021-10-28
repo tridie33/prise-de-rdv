d2ba5c49ad213d7bf8a58bf76cf2311f9e42505e
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2021-10-28 16:34:13 +0200

    feat(global): ui + emails updated (#86)
    * feat(server/src/http/routes/public/appointmentRequest.js): mail subject updated
    
    * feat(pages/formCandidat/layout/FooterComponent.js): footer updated
    
    * feat(server/src/assets/templates/mail-formation.mjml.ejs): template updated
    
    * feat(server/src/assets/templates/mail-candidat.mjml.ejs): template updated
    
    * feat(theme/components/input.js): height updated
    
    * feat(pages/formCandidat/layout/ContactCfaComponent.js): margin left added
    
    * feat(pages/formCandidat/layout/FormHeaderComponent.js): design updated
    
    * feat(pages/formCandidat/FormCreatePage.js): design updated
    
    * feat(pages/formCandidat/FormRecapPage.js): design + wording updated
    
    * feat(public/assets/people.png): file updated
    
    * feat(public/assets/people2.png): file added
    
    * feat(public/assets/people.svg): file removed
    
    * feat(theme/components/icons/IconeLogo.js): file updated

--- ui/src/pages/formCandidat/layout/FooterComponent.js
+++ ui/src/pages/formCandidat/layout/FooterComponent.js
@@ -6,10 +6,10 @@ import { Box, Text, Link } from "@chakra-ui/react";
  */
 export const FooterComponent = () => (
   <Box bg="#F9F8F6" p={8}>
-    <Text textAlign="center">
-      Un outil développé par la{" "}
+    <Text textAlign="center" fontSize="14px">
+      <b>RDV Apprentissage</b> est un outil développé par{" "}
       <Link borderBottom="1px solid #2A2A2A" href="https://mission-apprentissage.gitbook.io/general/">
-        Mission interministérielle pour l’apprentissage
+        la Mission interministérielle pour l’apprentissage
       </Link>
     </Text>
   </Box>
--- ui/src/pages/formCandidat/layout/FormHeaderComponent.js
+++ ui/src/pages/formCandidat/layout/FormHeaderComponent.js
@@ -5,19 +5,19 @@ export const FormHeaderComponent = () => {
   return (
     <Box bg="#F9F8F6">
       <Flex alignItems="center" flexDirection={["column", "column", "row"]}>
-        <Box flex="1" ml={["0", "0", "10rem"]}>
+        <Box flex="1" ml={["0", "0", "6em"]}>
           <Flex flexDirection={["column", "column", "row"]} mt={[7, 0, 0]}>
-            <MessageLogo w="48px" h="48px" mx="auto" />
-            <Text textStyle="h2" color="grey.700" textAlign={["center", "center", "unset"]} ml={3}>
-              Le centre de formation <br />
-              <Text textStyle="h2" as="span" color="info">
-                vous rappelle !
+            <Text textStyle="h2" color="info">
+              Envoyer <br />
+              une demande de contact <br />
+              <Text textStyle="h2" as="span" color="grey.700">
+                au centre de formation
               </Text>
             </Text>
           </Flex>
         </Box>
-        <Box mr="5rem" mt={8}>
-          <IconeLogo w={["0px", "0px", "240px"]} h={["0px", "0px", "196.4px"]} />
+        <Box mr="2rem" mt={8}>
+          <IconeLogo w={["0px", "0px", "300px"]} h={["0px", "0px", "174px"]} />
         </Box>
       </Flex>
     </Box>
