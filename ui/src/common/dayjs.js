255d71164d7370e51516a8cb49d398d76ab7f1bb
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2021-11-12 17:04:45 +0100

    feat(ui/src/common/dayjs.js): plugins added

--- ui/src/common/dayjs.js
+++ ui/src/common/dayjs.js
@@ -1,7 +1,21 @@
 import * as dayjs from "dayjs";
 import * as utc from "dayjs/plugin/utc";
+import * as duration from "dayjs/plugin/duration";
+import * as timezone from "dayjs/plugin/timezone";
+import * as isSameOrBefore from "dayjs/plugin/isSameOrBefore";
+import * as isSameOrAfter from "dayjs/plugin/isSameOrAfter";
+import * as isBetween from "dayjs/plugin/isBetween";
+import * as customParseFormat from "dayjs/plugin/customParseFormat";
+import * as advancedFormat from "dayjs/plugin/advancedFormat";
 
 dayjs.extend(utc);
+dayjs.extend(duration);
+dayjs.extend(timezone);
+dayjs.extend(isSameOrBefore);
+dayjs.extend(isSameOrAfter);
+dayjs.extend(isBetween);
+dayjs.extend(customParseFormat);
+dayjs.extend(advancedFormat);
 
 /**
  * @description Formats date.
