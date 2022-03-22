bfd8cb9fd14c6bd0c5c888ec91892fc1f4cc0e39
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2022-03-22 21:43:57 +0100

    tests(server/tests/utils/nockApis/nock-MnaCatalog.js): nock updated

--- server/tests/utils/nockApis/nock-MnaCatalog.js
+++ server/tests/utils/nockApis/nock-MnaCatalog.js
@@ -6,6 +6,17 @@ module.exports = async () => {
   nock(config.mnaCatalog.endpoint)
     .persist()
     .post("/v1/entity/formations", {
+      query: "{}",
+      select:
+        '{"code_postal":1,"id_rco_formation":1,"etablissement_formateur_entreprise_raison_sociale":1,"etablissement_gestionnaire_courriel":1,"etablissement_formateur_courriel":1,"intitule_long":1,"etablissement_formateur_adresse":1,"etablissement_formateur_code_postal":1,"etablissement_formateur_nom_departement":1,"etablissement_formateur_localite":1,"lieu_formation_adresse":1,"etablissement_formateur_siret":1,"etablissement_gestionnaire_siret":1,"cfd":1,"localite":1,"email":1,"published":1,"parcoursup_id":1,"cle_ministere_educatif":1}',
+      page: 1,
+      limit: 50,
+    })
+    .reply(200, sampleCatalogueResponse);
+
+  nock(config.mnaCatalog.endpoint)
+    .persist()
+    .post("/v1/entity/formations", {
       query: '{"$and":[{"id_rco_formation":["21_114876|21_114876|106291"]}]}',
       select:
         '{"code_postal":1,"id_rco_formation":1,"etablissement_formateur_entreprise_raison_sociale":1,"etablissement_gestionnaire_courriel":1,"etablissement_formateur_courriel":1,"intitule_long":1,"etablissement_formateur_adresse":1,"etablissement_formateur_code_postal":1,"etablissement_formateur_nom_departement":1,"etablissement_formateur_localite":1,"lieu_formation_adresse":1,"etablissement_formateur_siret":1,"etablissement_gestionnaire_siret":1,"cfd":1,"localite":1,"email":1,"published":1,"parcoursup_id":1,"cle_ministere_educatif":1}',
